6be2efd06789bc417bd3379bccbc605aefc97db6
Noela-Murugi <murugin254@gmail.com>
2022-12-07 22:47:08 +0300

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footerF text-center">
+      <a href="#myPage" title="To Top">
+        <span className="glyphicon glyphicon-chevron-up"></span>
+      </a>
+      <p> © 2022 Luxuro. Made By : Noela Murugi</p>
+    </footer>
+  );
+}
+export default Footer;
