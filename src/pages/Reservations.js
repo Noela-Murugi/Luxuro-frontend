8870ff30b63acbd95a7c8d6b97c5ecc3cd5f49ba
Noela-Murugi <murugin254@gmail.com>
2022-12-07 22:40:34 +0300

    reservations list page

---
+++ src/pages/Reservations.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Box, Button } from "../styles";
+
+function Reservations() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch("/recipes")
+      .then((r) => r.json())
+      .then(setRecipes);
+  }, []);
+
+  return (
+    <Wrapper>
+      {recipes.length > 0 ? (
+        recipes.map((recipe) => (
+          <Recipe key={recipe.id}>
+            <Box>
+              <h2>{recipe.title}</h2>
+              <p>
+                <em>Number of days: {recipe.minutes_to_complete} days</em>
+                &nbsp;·&nbsp;
+                <cite>By {recipe.user.username}</cite>
+              </p>
+              <ReactMarkdown>{recipe.instructions}</ReactMarkdown>
+            </Box>
+          </Recipe>
+        ))
+      ) : (
+        <>
+          <h2>No Reservations Found</h2>
+          <Button as={Link} to="/new">
+            Make a New Reservation
+          </Button>
+        </>
+      )}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  max-width: 800px;
+  margin: 40px auto;
+`;
+
+const Recipe = styled.article`
+  margin-bottom: 24px;
+`;
+
+export default Reservations;
