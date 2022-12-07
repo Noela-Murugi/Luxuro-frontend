1abb7fbc3b2e79686ac5a92dca3c371df7242234
Noela-Murugi <murugin254@gmail.com>
2022-12-07 22:47:29 +0300

    add details component

---
+++ src/components/Details.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import NavBar from "../nav/NavBar"
+import { Link } from "react-router-dom";
+import { Button } from "../styles";
+
+export default function JobDetails() {
+
+
+    return (
+        <>
+        <NavBar/>
+
+        <div className='details'>
+            <h2>Station</h2>
+            <div className='job-details'>
+                <div className='job-details-image'>
+                </div>
+                <div className='job-details-content'>
+                    <h2>tt</h2>
+                    <h3>Job Requirement: </h3>
+                    <h3>Location:</h3>
+                    <h3>Salary: </h3>
+                    <h3>Application Deadline:</h3>
+                </div>
+            </div>
+            <div className='action-buttons'>
+            <Button as={Link} to="/new" >Reserve</Button>
+            </div>
+        </div>
+        </>
+  )
+}
