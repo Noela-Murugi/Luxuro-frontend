4abb8ceffba6aec85a042e81960a100164580bc0
Noela-Murugi <murugin254@gmail.com>
2022-12-09 14:27:28 +0300

    done

--- src/components/Details.js
+++ src/components/Details.js
@@ -12,10 +12,6 @@ export default function JobDetails() {
 
         <div className='details'>
             {/* <h2>Station</h2> */}
-            <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/142929487.jpg?k=0279ec440bb7ac3d77719dcfa65ccdc8de8fdb37c45e11008dfda29844eab223&o=&hp=1" alt="job" />
-            <div className='job-details'>
-                <div className='job-details-image'>
-                </div>
                 <div className='job-details-content'>
                     <h3>Capacity: 20 - 50 people </h3>
                     <h3>Features:<br/>
@@ -29,7 +25,7 @@ export default function JobDetails() {
             <div className='action-buttons'>
             <Button as={Link} to="/new" >Reserve</Button>
             </div>
-        </div>
+        {/* </div> */}
         </>
   )
 }
