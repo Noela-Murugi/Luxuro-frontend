a3befe860a4bd003c0a0f35ddccc60c61a01e3ef
Noela-Murugi <murugin254@gmail.com>
2022-12-09 13:58:11 +0300

    short description

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,7 @@ function Home(){
         <h1 >
         Welcome to <span className="anything">LUXURO </span> <br></br>
         </h1>
-        <h2>A hotel resort. Make your reservations today!!</h2>
+        <h3 style={{color:'white'}}>A hotel resort. Make your reservations today!!</h3>
         <Button as={Link} to="/new" >Reserve</Button>
       </div>
       <div className="hero-image">
